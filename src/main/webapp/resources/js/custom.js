23cc85f8ac6e6f73ee88a9124aae3754c919bf6e
Danieil Skrinikov <skrinikovd@gmail.com>
2017-02-07 15:43:31 -0500

    A lot of changes

--- src/main/webapp/resources/js/custom.js
+++ src/main/webapp/resources/js/custom.js
@@ -1,40 +1,40 @@
 $(document).ready(function () {
-  var trigger = $('.hamburger'),
-      overlay = $('.overlay'),
-     isClosed = false;
+    var trigger = $('.hamburger'),
+            overlay = $('.overlay'),
+            isClosed = false;
 
     trigger.click(function () {
-      hamburger_cross();      
+        hamburger_cross();
     });
 
     function hamburger_cross() {
 
-      if (isClosed == true) {          
-        overlay.hide();
-        trigger.removeClass('is-open');
-        trigger.addClass('is-closed');
-        isClosed = false;
-      } else {   
-        overlay.show();
-        trigger.removeClass('is-closed');
-        trigger.addClass('is-open');
-        isClosed = true;
-      }
-  }
-  
-  $('[data-toggle="offcanvas"]').click(function () {
+        if (isClosed == true) {
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+        } else {
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+        }
+    }
+
+    $('[data-toggle="offcanvas"]').click(function () {
         $('#wrapper').toggleClass('toggled');
-  });
-  
-  $('a[href="#navbar-more-show"], .navbar-more-overlay').on('click', function(event) {
-		event.preventDefault();
-		$('body').toggleClass('navbar-more-show');
-		if ($('body').hasClass('navbar-more-show'))	{
-			$('a[href="#navbar-more-show"]').closest('li').addClass('active');
-		}else{
-			$('a[href="#navbar-more-show"]').closest('li').removeClass('active');
-		}
-		return false;
-	});
+    });
+
+    $('a[href="#navbar-more-show"], .navbar-more-overlay').on('click', function (event) {
+        event.preventDefault();
+        $('body').toggleClass('navbar-more-show');
+        if ($('body').hasClass('navbar-more-show')) {
+            $('a[href="#navbar-more-show"]').closest('li').addClass('active');
+        } else {
+            $('a[href="#navbar-more-show"]').closest('li').removeClass('active');
+        }
+        return false;
+    });
 });
 
