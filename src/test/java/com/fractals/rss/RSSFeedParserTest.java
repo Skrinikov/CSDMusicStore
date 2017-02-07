9dbe0b56ed1631c6bb6767a9c8998ead4235643b
Danieil Skrinikov <skrinikovd@gmail.com>
2017-02-07 16:58:46 -0500

    Add RSS undone

---
+++ src/test/java/com/fractals/rss/RSSFeedParserTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fractals.rss;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 1430468
+ */
+public class RSSFeedParserTest {
+    
+    public RSSFeedParserTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of readFeed method, of class RSSFeedParser.
+     */
+    @Test
+    public void testReadFeed() {
+        System.out.println("readFeed");
+        RSSFeedParser instance = new RSSFeedParser("http://rss.cbc.ca/lineup/topstories.xml");
+        Feed expResult = null;
+        Feed result = instance.readFeed();
+        System.out.println(result);
+        fail("The test case is a prototype.");
+    }
+    
+}
