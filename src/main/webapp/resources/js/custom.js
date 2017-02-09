cc9bf2a9107535abb731e67e3c54292d95285018
Skrinikov <skrinikovd@gmail.com>
2017-02-08 20:58:15 -0500

    Fix client NavBar.Style footer. For some reason when the menu is a ui component its animation does not work.

--- src/main/webapp/resources/js/custom.js
+++ src/main/webapp/resources/js/custom.js
@@ -1,32 +1,15 @@
 $(document).ready(function () {
-    var trigger = $('.hamburger'),
-            overlay = $('.overlay'),
-            isClosed = false;
-
-    trigger.click(function () {
-        hamburger_cross();
+    
+    $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
     });
-
-    function hamburger_cross() {
-
-        if (isClosed == true) {
-            overlay.hide();
-            trigger.removeClass('is-open');
-            trigger.addClass('is-closed');
-            isClosed = false;
-        } else {
-            overlay.show();
-            trigger.removeClass('is-closed');
-            trigger.addClass('is-open');
-            isClosed = true;
-        }
-    }
 
     $('[data-toggle="offcanvas"]').click(function () {
         $('#wrapper').toggleClass('toggled');
     });
 
     $('a[href="#navbar-more-show"], .navbar-more-overlay').on('click', function (event) {
+        console.log("Test");
         event.preventDefault();
         $('body').toggleClass('navbar-more-show');
         if ($('body').hasClass('navbar-more-show')) {
