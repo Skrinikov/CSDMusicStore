676ef84dcade1a78fa27157a515bfa399ae8df48
Thai-Vu Nguyen <thaivu22@gmail.com>
2017-02-12 20:35:19 -0500

    Album View. Did not extend the Master template yet.

---
+++ src/main/java/com/fractals/backingbeans/AlbumsClientBacking.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fractals.backingbeans;
+
+import com.fractals.beans.Album;
+import com.fractals.beans.Artist;
+import com.fractals.beans.Track;
+import com.fractals.controllers.AlbumJpaController;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ * The Backing bean for the client side Album page
+ * 
+ * @author Thai-Vu Nguyen
+ */
+@Named("albumsCLBack")
+@RequestScoped
+public class AlbumsClientBacking {
+    private Album album;
+    private Integer albumId;
+    
+    @Inject
+    AlbumJpaController albumControl;
+    
+    //Initializes the Album entity
+    public void init(){
+         album = albumControl.findAlbum(albumId);
+    }
+    
+    public Album getAlbum(){
+        return album;
+    }
+    
+    public Integer getAlbumId(){
+        return this.albumId;
+    }
+    
+    public void setAlbumId(Integer albumId){
+        this.albumId = albumId;
+    }
+    
+    public String getTitle(){
+        return (album != null)?album.getTitle():"";
+    }
+    
+    public Artist getArtist(){      
+        return (album != null)?album.getArtist():(new Artist());
+    }
+    
+    public boolean isAvailable(){
+        return (album != null)?album.getAvailable():false;
+    }
+    
+    public double getCostPrice(){
+        return (album != null)?album.getCostPrice():0.00;
+    }
+    
+    public double getListPrice(){
+        return (album != null)?album.getListPrice():0.00;
+    }
+    
+    public double getSalesPrice(){
+        return (album != null)?album.getSalePrice():0.00;
+    }
+    
+    public LocalDateTime getCreatedAt(){
+        return (album != null)?album.getCreatedAt():null;
+    }
+    
+    public List<Track> getTracks(){
+        return (album != null)?album.getTracks():(new ArrayList<Track>());
+    }
+    
+    public String getLabel(){
+        return (album != null)?album.getRecordLabel():"";
+    }
+     
+}
