e223d1c035773d595cfcdfca318377943b9132e6
Skrinikov <skrinikovd@gmail.com>
2017-02-18 12:30:25 -0500

    Work on index.xhtml

--- src/test/java/com/fractals/rss/RSSFeedParserTest.java
+++ src/test/java/com/fractals/rss/RSSFeedParserTest.java
@@ -1,6 +1,7 @@
 package com.fractals.rss;
 
 import java.util.List;
+import javax.xml.stream.XMLStreamException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -30,12 +31,12 @@ public class RSSFeedParserTest {
      * Test of readFeed method, of class RSSFeedParser.
      */
     @Test
-    public void testReadFeed() {
+    public void testReadFeed() throws XMLStreamException {
         System.out.println("readFeed");
         RSSFeedParser instance = new RSSFeedParser();
         
-        List<FeedMessage> result = instance.readFeed(new String[]{"http://rss.cbc.ca/lineup/topstories.xml"});
-        //System.out.println(result);
+        List<FeedMessage> result = instance.readFeed(new String[]{"http://apps.shareholder.com/rss/rss.aspx?channels=632&companyid=YHOO&sh_auth=3913894844%2E0%2E0%2E42785%2Eb0bacaa66e68a37fe8bf3bcb9066fdfd"});
+        System.out.println(result);
         assertEquals(1,1);
     }
     
