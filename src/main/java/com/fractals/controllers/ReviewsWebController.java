f231bc8dbe09bdad036bc0de256faf41ec57d9ac
Thai-Vu Nguyen <thaivu22@gmail.com>
2017-02-19 11:47:30 -0500

    cleaned up the reviews controller to interact with the generated controller

--- src/main/java/com/fractals/controllers/ReviewsWebController.java
+++ src/main/java/com/fractals/controllers/ReviewsWebController.java
@@ -6,12 +6,14 @@ import com.fractals.beans.Track;
 import com.fractals.beans.User;
 import java.io.Serializable;
 import java.sql.Date;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.Resource;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -29,14 +31,9 @@ import javax.transaction.UserTransaction;
 @Named("reviewsController")
 @RequestScoped
 public class ReviewsWebController implements Serializable {
-    
-    public ReviewsWebController(){}
-    
-    @Resource
-    private UserTransaction userTransaction;
 
-    @PersistenceContext(unitName = "fractalsPU")
-    private EntityManager entityManager;
+    @Inject
+    ReviewJpaController reviewControl;
     
     /**
      * Submits a review by a user for a track
@@ -46,69 +43,34 @@ public class ReviewsWebController implements Serializable {
      * @param rating int
      * @return Review
      */
-    public Review addReview(Track track, String message, User user, int rating){
-        Review review = createReview(message, user, rating);
-        updateReviewsInTrack(track, review);
-        return review;
+    public boolean addReview(Track track, String message, User user, int rating){
+        try {
+            Review review = new Review();
+            
+            reviewControl.create(review);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+        
     }
     
     /**
-     * Creates an instance of Review in the database
+     * Fills the Review bean
+     * @param track Track
      * @param message String
      * @param user User
      * @param rating int
-     * @return a new Review instance created in the database
+     * @return Review
      */
-    private Review createReview (String message, User user, int rating){
+    private Review buildBean(Track track, String message, User user, int rating){
         Review review = new Review();
+        review.setRating(rating);
+        review.setTrack(track);
         review.setUser(user);
         review.setText(message);
-        review.setApproved(false);
-        review.setRating(rating);
         review.setReviewDate(LocalDateTime.now());
-        
-        try{
-            userTransaction.begin();
-            
-            entityManager.persist(review);
-            
-            userTransaction.commit();
-            review = entityManager.find(Review.class, review.getId());
-            entityManager.refresh(review);
-        }catch (NotSupportedException | SystemException | RollbackException | HeuristicMixedException | 
-               HeuristicRollbackException | SecurityException | IllegalStateException ex) {
-            try {
-                
-                userTransaction.rollback();
-            } 
-            catch (IllegalStateException | SecurityException | SystemException re) {}
-            return null;
-        }
-        
+        review.setApproved(false);
         return review;
     }
-
-    /**
-     * Updates the list of reviews in a track
-     * 
-     * @param track Track
-     * @param review Review
-     */
-    private void updateReviewsInTrack(Track track, Review review) {
-        try {
-            
-            userTransaction.begin();
-            List<Review> reviews = track.getReviews();
-            reviews.add(review);
-            track.setReviews(reviews);
-            entityManager.persist(track);
-            userTransaction.commit();
-        } catch (NotSupportedException | SystemException | RollbackException | HeuristicMixedException | 
-               HeuristicRollbackException | SecurityException | IllegalStateException ex) {
-            try {
-                userTransaction.rollback();
-            } 
-            catch (IllegalStateException | SecurityException | SystemException re) {}            
-        }
-    }    
 }
