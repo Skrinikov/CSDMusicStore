54dba757ffe8d0d98e6e13d108e8f5fe9b46fbe8
Sarah <Sarah@PC-Sarah>
2017-02-19 03:10:36 +0100

    Worked on delete and editing of album, delete works, but not with the confirm dialog, also worked on editing

--- src/main/java/com/fractals/backingbeans/AlbumBackingBean.java
+++ src/main/java/com/fractals/backingbeans/AlbumBackingBean.java
@@ -8,7 +8,6 @@ package com.fractals.backingbeans;
 import com.fractals.beans.Album;
 import com.fractals.beans.Artist;
 import com.fractals.controllers.AlbumJpaController;
-import com.fractals.controllers.ArtistJpaController;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -33,46 +32,28 @@ public class AlbumBackingBean {
     public Artist getTest(){return test;}
     public void setTest(Artist stuff){test=stuff;}
    
+    private boolean editable = false;
+    public void setEditable(boolean b) {editable = b;}
+    public boolean getEditable() {return editable;}
+    public void editable() throws Exception {setEditable(true);}
+    public void uneditable() {setEditable(false);}
+
     private Album selectedAlbum;
     
-    private boolean editable = false;
-
-    public void setEditable(boolean b) {
-        editable = b;
-    }
-
-    public boolean getEditable() {
-        return editable;
-    }
-
-    public void edit() {
-        setEditable(true);
-    }
-
-    public void unedit() {
-        setEditable(false);
-    }
-
     public Album getSelectedAlbum() {
         if(selectedAlbum == null)
-            selectedAlbum = new Album();   
-        return selectedAlbum;
+            selectedAlbum = getFirst();   //A CHANGER
+        return selectedAlbum;      
     }
     
     public void setSelectedAlbum(Album a) {
         selectedAlbum = a;
     }
 
-    public Album getFirst() {
-       return getAlbums().get(0);
-    }
+    private Album getFirst() {return getAlbums().get(72);}
 
     @Inject
-    private AlbumJpaController albumJpaController;
-    @Inject
-    private ArtistJpaController artistJpaController;
-
-    
+    private AlbumJpaController albumJpaController;  
     
     public List<Album> getAlbums() {
         return albumJpaController.findAlbumEntities();
@@ -85,7 +66,16 @@ public class AlbumBackingBean {
     public void create() throws Exception {
         selectedAlbum.setCreatedAt(LocalDateTime.now());
         selectedAlbum.setReleaseDate(LocalDate.now());//A CHANGER
-       // selectedAlbum.setArtist(getAlbums().get(0).getArtist());
+        selectedAlbum.setArtist(getAlbums().get(0).getArtist());//A CHANGER
         albumJpaController.create(selectedAlbum);
+    }
+    
+    public void edit() throws Exception{
+        albumJpaController.edit(selectedAlbum);
+    }
+    
+    public void delete() throws Exception{
+        albumJpaController.destroy(selectedAlbum.getId()); 
+        selectedAlbum = null;
     }
 }
