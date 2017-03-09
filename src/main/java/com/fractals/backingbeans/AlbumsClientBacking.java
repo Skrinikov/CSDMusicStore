e3af3a42f4940e1695c0aabdc57ae0b4c8a6ca06
Danieil Skrinikov <skrinikovd@gmail.com>
2017-03-09 09:43:43 -0500

    Add fragments to albums

--- src/main/java/com/fractals/backingbeans/AlbumsClientBacking.java
+++ src/main/java/com/fractals/backingbeans/AlbumsClientBacking.java
@@ -8,12 +8,15 @@ import com.fractals.controllers.AlbumJpaController;
 import com.fractals.controllers.ClientTrackingController;
 import com.fractals.controllers.LoginController;
 import com.fractals.controllers.ReviewsWebController;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
@@ -21,71 +24,88 @@ import javax.inject.Named;
 
 /**
  * The Backing bean for the client side Album page
- * 
+ *
  * @author Thai-Vu Nguyen, Danieil Skrinikov
  */
 @Named("albumsCLBack")
-@RequestScoped
-public class AlbumsClientBacking {
+@SessionScoped
+public class AlbumsClientBacking implements Serializable{ 
+
     private Album album;
     private Integer albumId;
     private List<Album> similarAlbums;
     private boolean isLoaded = false;
     private Track selectedTrack;
     private Review createdReview;
-    
+
+    private static final Logger log = Logger.getLogger("AlbumsClientBacking.class");
+
     @Inject
     AlbumJpaController albumControl;
-    
+
     @Inject
     ShoppingCart shopControl;
-    
-    @Inject 
+
+    @Inject
     ReviewsWebController reviewControl;
-    
+
     @Inject
     private LoginController loginControl;
-    
+
     @Inject
     private ClientTrackingController cookiesControl;
-    
+
     //Initializes the Album entity
-    public void init(){
-         album = albumControl.findAlbum(albumId);
-         similarAlbums = albumControl.getSimilarAlbums(album, 3);
-         
-         cookiesControl.registerGenreToCookies(album.getTracks().get(0).getGenre());
-         isLoaded=true;
+    public void init() {
+        album = albumControl.findAlbum(albumId);
+
+        if (album == null) {
+            log.info("Album is NULL");
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            String outcome = "/temp.xhtml"; // Maybe change to a Album 404 page.
+            facesContext.getApplication().getNavigationHandler().handleNavigation(facesContext, null, outcome);
+        } else {
+
+            similarAlbums = albumControl.getSimilarAlbums(album, 3);
+
+            // Thai Vu I added this, because some albums have no tracks.
+            if (album.getTracks().size() > 0) {
+                cookiesControl.registerGenreToCookies(album.getTracks().get(0).getGenre());
+            }
+            isLoaded = true;
+        }
     }
-    
+
     /**
      * Function to add the current instance of the album to the shopping
      */
-    public void addAlbumToCart(){
+    public void addAlbumToCart() {
         shopControl.add(album);
     }
-    
-    public Album getAlbum(){
+
+    public Album getAlbum() {
         return album;
     }
-    
-    public Integer getAlbumId(){
+
+    public Integer getAlbumId() {
         return this.albumId;
     }
-    
-    public Review getReview(){
-        if (createdReview == null)
+
+    public Review getReview() {
+        if (createdReview == null) {
             createdReview = new Review();
+        }
         return createdReview;
     }
-    
-    public Track getSelectedTrack(){
-        if (selectedTrack == null)
+
+    public Track getSelectedTrack() {
+        if (selectedTrack == null) {
             selectedTrack = new Track();
+        }
         return selectedTrack;
     }
-    
-    public void setAlbumId(Integer albumId){
+
+    public void setAlbumId(Integer albumId) {
         this.albumId = albumId;
     }
 
@@ -95,7 +115,7 @@ public class AlbumsClientBacking {
 
     public void setSimilarAlbums(List<Album> similarAlbums) {
         this.similarAlbums = similarAlbums;
-    }     
+    }
 
     public boolean isIsLoaded() {
         return isLoaded;
@@ -104,88 +124,96 @@ public class AlbumsClientBacking {
     public void setIsLoaded(boolean isLoaded) {
         this.isLoaded = isLoaded;
     }
-    
-    public void setSelectedTrack(Track selectedTrack){
+
+    public void setSelectedTrack(Track selectedTrack) {
         this.selectedTrack = selectedTrack;
     }
-       
-    
+
     /**
-     * Returns the real price for the album. If there is a sale, returns the sale price, if not returns the list price.
-     * 
+     * Returns the real price for the album. If there is a sale, returns the
+     * sale price, if not returns the list price.
+     *
      * @param album Album to fetch the information from.
      * @return Real price for the album.
      */
-    public double getPrice(Album album){
+    public double getPrice(Album album) {
         return (album.getSalePrice() <= 0) ? album.getListPrice() : album.getSalePrice();
     }
-    
+
     /**
      * returns the cover image name of the album.
-     * @return 
+     *
+     * @return
      */
-    public String getAlbumCover(){   
-        return album.getTracks().get(0).getCoverFile();     
+    public String getAlbumCover() {
+        if(!album.getTracks().isEmpty())
+            return album.getTracks().get(0).getCoverFile();
+        else
+            return "2001.jpg";
     }
-    
-    public String getAlbumCover(Album album){
+
+    public String getAlbumCover(Album album) {
         return album.getTracks().get(0).getCoverFile();
     }
-    
-    public String addReview(){
+
+    public String addReview() {
         Review review = getReview();
-        
-        if(loginControl.isLoggedIn())
+
+        if (loginControl.isLoggedIn()) {
             review.setUser(loginControl.getCurrentUser());
-        else
+        } else {
             return "/index.xhtml";
+        }
         review.setApproved(false);
         review.setReviewDate(LocalDateTime.now());
-        
+
         reviewControl.addReview(review);
-        
+
         return "Album.xhtml?faces-redirect=true&id=" + albumId.intValue();
-        
+
     }
-    
-    public void showReviewDialog(){
+
+    public void showReviewDialog() {
         Map<String, Object> options = new HashMap<String, Object>();
         options.put("modal", true);
         options.put("draggable", false);
         options.put("resizable", false);
         options.put("includeViewParams", true);
-        
+
         //TODO
-        
         //TESTING if the dialog pops out
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Hello - ", null);
         FacesContext.getCurrentInstance().addMessage(null, message);
     }
-    
-    public boolean isLoggedIn(){
+
+    public boolean isLoggedIn() {
         return loginControl.isLoggedIn();
     }
-    
-    private boolean canBuyAlbum(){
+
+    private boolean canBuyAlbum() {
         List<Album> albumsInCart = shopControl.getAllAlbums();
-        if (albumsInCart == null || albumsInCart.isEmpty())
+        if (albumsInCart == null || albumsInCart.isEmpty()) {
             return true;
-        
-        if (albumsInCart.contains(album))
+        }
+
+        if (albumsInCart.contains(album)) {
             return false;
-        else
+        } else {
             return true;
+        }
     }
-    
-    private boolean canBuyTrack(){
+
+    private boolean canBuyTrack() {
         List<Track> tracksInCart = shopControl.getAllTracks();
-        if (tracksInCart == null || tracksInCart.isEmpty())
+        if (tracksInCart == null || tracksInCart.isEmpty()) {
             return true;
-        
-        if (tracksInCart.contains(selectedTrack))
+        }
+
+        if (tracksInCart.contains(selectedTrack)) {
             return false;
-        else
+        } else {
             return true;
+        }
     }
 
 }
--- src/main/webapp/resources/js/custom.js
+++ src/main/webapp/resources/js/custom.js
@@ -43,6 +43,7 @@ $(document).ready(function () {
 
         $(this).html(stars);
     });
+    
 
 });
 
