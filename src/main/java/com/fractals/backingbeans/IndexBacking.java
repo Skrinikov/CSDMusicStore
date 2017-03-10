8dee8cab6276d1efddc116296cf1fba7760d1732
Thai-Vu Nguyen <thaivu22@gmail.com>
2017-03-09 20:17:57 -0500

    Client Backing implementation on the Index page. Doesn't work with un-authenticated users yet

--- src/main/java/com/fractals/backingbeans/IndexBacking.java
+++ src/main/java/com/fractals/backingbeans/IndexBacking.java
@@ -1,7 +1,9 @@
 package com.fractals.backingbeans;
 
+import com.fractals.beans.Album;
 import com.fractals.beans.Genre;
 import com.fractals.beans.Track;
+import com.fractals.controllers.ClientTrackingController;
 import com.fractals.controllers.GenreJpaController;
 import com.fractals.controllers.RssFeedController;
 import com.fractals.controllers.TrackJpaController;
@@ -16,6 +18,7 @@ import javax.inject.Named;
  * This bean is used by the index page to fetch important data from various controllers.
  * 
  * @author Danieil Skrinikov
+ * @author Thai-Vu Nguyen
  * @version 1.0.0
  * @since 2017-02-17
  */
@@ -31,10 +34,16 @@ public class IndexBacking {
     @Inject
     private TrackJpaController tracks;
     
+    @Inject 
+    private ClientTrackingController tracker;
+    
     // Class Variables
     private List<FeedMessage> rssMsgs;
     private List<Genre> genres;
     private List<Track> newTracks;
+    private List<Track> trackedTracks;
+    private List<Album> trackedAlbums;
+    private Genre trackedGenre;
     
     /**
      * Generates all components which are needed to display the page.
@@ -43,9 +52,22 @@ public class IndexBacking {
     public void init(){
         rssMsgs = rss.getVisibleFeed();
         genres = genre.findGenreEntities();
-        newTracks = tracks.getMostRecentTracks(3);
+        newTracks = tracks.getMostRecentTracks(3); 
     }
     
+    /**
+     * Loading Tracked genre, albums, songs here because it
+     * doesn't wanna work with PostConstruct
+     * 
+     * @author Thai-Vu Nguyen
+     */
+    public void load(){
+        if (tracker.isGenreSaved()){
+            trackedGenre = tracker.getGenre();
+            trackedAlbums = tracker.getAlbums(3, true);
+            trackedTracks = tracker.getTracks(3, true);
+        }
+    }
     /* Getters and Setters */
 
     public List<FeedMessage> getRssMsgs() {
@@ -71,7 +93,33 @@ public class IndexBacking {
     public void setNewTracks(List<Track> newTracks) {
         this.newTracks = newTracks;
     }
-   
+
+    public List<Track> getTrackedTracks() {
+        return trackedTracks;
+    }
+
+    public void setTrackedTracks(List<Track> trackedTracks) {
+        this.trackedTracks = trackedTracks;
+    }
+
+    public List<Album> getTrackedAlbums() {
+        return trackedAlbums;
+    }
+
+    public void setTrackedAlbums(List<Album> trackedAlbums) {
+        this.trackedAlbums = trackedAlbums;
+    }
+
+    public Genre getTrackedGenre() {
+        return trackedGenre;
+    }
+
+    public void setTrackedGenre(Genre trackedGenre) {
+        this.trackedGenre = trackedGenre;
+    }
     
- 
+    public boolean isGenreSaved(){
+        return tracker.isGenreSaved();
+    }
+
 }
