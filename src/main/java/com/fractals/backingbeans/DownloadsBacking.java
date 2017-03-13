01077b561b364bcfeabec679afe72af199d4e2fa
Skrinikov <skrinikovd@gmail.com>
2017-03-12 21:20:22 -0500

    Modify DownloadsController

---
+++ src/main/java/com/fractals/backingbeans/DownloadsBacking.java
@@ -0,0 +1,62 @@
+package com.fractals.backingbeans;
+
+import com.fractals.beans.Track;
+import com.fractals.controllers.DownloadListController;
+import java.io.Serializable;
+import java.util.List;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ * This bean contains all the necessary logic to display the downloadable files 
+ * for a given user.
+ * 
+ * 
+ * @author Danieil Skrinikov
+ * @version 1.0.01
+ * 
+ */
+@Named
+@SessionScoped
+public class DownloadsBacking implements Serializable{
+    
+    private static transient final java.util.logging.Logger log = java.util.logging.Logger.getLogger(DownloadsBacking.class.getName());
+    
+    @Inject
+    private LoginBacking login;
+    
+    @Inject
+    private DownloadListController downloadController;
+    
+    private List<Track> downloadableTracks;
+    
+    /**
+     * Initializes all components required to display the page and makes sure the
+     * person accessing this page is authenticated.
+     */
+    public void init(){
+        if(!login.isLoggedIn()){
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            String outcome = "login.xhtml";
+            facesContext.getApplication().getNavigationHandler().handleNavigation(facesContext, null, outcome);
+        }
+        
+        downloadableTracks = downloadController.getClientTracks(login.getCurrentUser());
+    }
+
+    
+    /*
+     * Getters and setters.
+     */
+    
+    public List<Track> getDownloadableTracks() {
+        return downloadableTracks;
+    }
+
+    public void setDownloadableTracks(List<Track> downloadableTracks) {
+        this.downloadableTracks = downloadableTracks;
+    }
+    
+}
